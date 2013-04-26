c9f5ab5ef09be70ed07e0693647ef77a80583be9
rachelss <rsschwartz@gmail.com>
2013-04-26 15:00:49 -0700

    Update make_pruned_dict.py
    Published version 1

--- make_pruned_dict.py
+++ make_pruned_dict.py
@@ -5,14 +5,16 @@ import cPickle
 
 allbases=dict()
 path=sys.argv[1]
-for file in os.listdir(path):
-    if file.endswith("pileups"):
-        filein=open(path+'/'+file,'r')
+minread=sys.argv[2]
+minread=int(minread)
+for fi in os.listdir(path):
+    if fi.endswith("pileups"):
+        filein=open(path+'/'+fi,'r')
         for line in filein.xreadlines():
             node,pos,ref,num,bases,qual=line.split()
-            bases=bases.replace('.',ref)            #insert ref base
+            bases=bases.replace('.',ref) #insert ref base
             bases=bases.replace(',',ref)
-            bases=bases.upper()     #everything in uppercase
+            bases=bases.upper() #everything in uppercase
             bases=list(bases)
             loc=node+'/'+pos
             if loc in allbases:
@@ -23,19 +25,19 @@ for file in os.listdir(path):
 
 #prune by whether there's enough info and species are fixed
 for loc in allbases.iterkeys():
-    if((allbases[loc].count('A')+allbases[loc].count('C')+allbases[loc].count('G')+allbases[loc].count('T'))< 3):   #not enough info
+    if((allbases[loc].count('A')+allbases[loc].count('C')+allbases[loc].count('G')+allbases[loc].count('T'))< minread): #not enough info
         allbases[loc]='N'
     else:
-        if(bool(allbases[loc].count('A'))+bool(allbases[loc].count('C'))+bool(allbases[loc].count('G'))+bool(allbases[loc].count('T')))>1:  #not fixed
+        if(bool(allbases[loc].count('A'))+bool(allbases[loc].count('C'))+bool(allbases[loc].count('G'))+bool(allbases[loc].count('T')))>1: #not fixed
             allbases[loc]='N'
-        else:                       #set base for this species
-            if(allbases[loc].count('A')>0):     
+        else: #set base for this species
+            if(allbases[loc].count('A')>0):
                 allbases[loc]='A'
-            elif(allbases[loc].count('C')>0):     
+            elif(allbases[loc].count('C')>0):
                 allbases[loc]='C'
-            elif(allbases[loc].count('G')>0):     
+            elif(allbases[loc].count('G')>0):
                 allbases[loc]='G'
-            elif(allbases[loc].count('T')>0):     
+            elif(allbases[loc].count('T')>0):
                 allbases[loc]='T'
 
 output = open(path+'/pruned_dict.pkl', 'wb')
