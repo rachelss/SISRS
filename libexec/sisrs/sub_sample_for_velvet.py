a647f553769de71a98ae44d9450f3fcbb9b5752e
rachelss <rsschwartz@gmail.com>
2014-07-12 19:34:47 -0700

    edited for python version compatibility

--- libexec/sisrs/sub_sample_for_velvet.py
+++ libexec/sisrs/sub_sample_for_velvet.py
@@ -9,17 +9,19 @@ i=0
 
 for fq in glob.glob(sys.argv[2]+"/*shuffled*fastq"):
     print 'Subsampling '+fq
-    with open(fq, 'r') as f:
-        for line in f:
-            if not line: break      #have less than desired coverage
-            read_info=[line,f.next(),f.next(),f.next(),f.next(),f.next(),f.next(),f.next()]
-            if i < N:        
-                sample.append(read_info)
-            elif random.random() < N/float(i+1):
-                replace = random.randint(0,len(sample)-1)
-                sample[replace] = read_info
-            i+=1
+    f = open(fq,'r')
+    for line in f:
+        if not line: break      #have less than desired coverage
+        read_info=[line,f.next(),f.next(),f.next(),f.next(),f.next(),f.next(),f.next()]
+        if i < N:        
+            sample.append(read_info)
+        elif random.random() < N/float(i+1):
+            replace = random.randint(0,len(sample)-1)
+            sample[replace] = read_info
+        i+=1
+    f.close()
 
-with open(sys.argv[2]+'/subsampled_shuffled.fastq','w') as f:
-    for read_info in sample:
-        f.write("".join(read_info))
+f = open(sys.argv[2]+'/subsampled_shuffled.fastq','w')
+for read_info in sample:
+    f.write("".join(read_info))
+f.close()
