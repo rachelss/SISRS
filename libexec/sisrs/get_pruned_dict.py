28d5e3ba08162fdfcf0427d3cef535d985a5ef37
rachelss <rsschwartz@gmail.com>
2015-02-22 17:32:10 -0700

    now using pysam

--- libexec/sisrs/get_pruned_dict.py
+++ libexec/sisrs/get_pruned_dict.py
@@ -3,80 +3,30 @@ import os
 import sys
 import cPickle
 from collections import Counter
+import pysam
 
 #get combined pileup info
-def getallbases(path):
+def getallbases(path,minread,thresh):
     allbases=dict()
     for fi in os.listdir(path):
-        if fi.endswith("pileups"):
-            filein=open(path+'/'+fi,'r')
-            for line in filein:
-                splitline=line.split()
-                if len(splitline)>4:
-                    node,pos,ref,num,bases,qual=line.split()
-                    bases=bases.replace('.',ref) #insert ref base
-                    bases=bases.replace(',',ref)
-                    bases=bases.upper() #everything in uppercase
-                    bases=list(bases)
-                    loc=node+'/'+pos
-                    if loc in allbases:
-                        allbases[loc].extend(bases)
-                    else:
-                        allbases[loc]=bases
-            filein.close()
+        if fi.endswith("bam"):
+            bamfile = pysam.AlignmentFile(path+'/'+fi, "rb" )
+            for pileupcolumn in bamfile.pileup():           #only doing one at a time
+                basesall=[pileupread.alignment.query_sequence[pileupread.query_position] for pileupread in pileupcolumn.pileups]       #get bases per site
+                bases=[b for b in basesall if b in ['a','c','g','t','A','C','G','T']]
+                if(len(bases)< minread):                                                #not enough info
+                    allbases[str(bamfile.getrname(pileupcolumn.tid))+str(pileupcolumn.pos)]='N'      #node_pos:'N'
+                elif Counter(bases).most_common(1)[0][1] / float(len(bases)) >= thresh: #enough of one
+                    return bamfile.getrname(pileupcolumn.tid),pileupcolumn.pos,Counter(bases).most_common(1)[0][0] #node_pos:base
+                else:       #het or lots of error
+                    return bamfile.getrname(pileupcolumn.tid),pileupcolumn.pos,'N'      #node_pos:'N'
+        
+            bamfile.close()
     
     return allbases
 
-#prune by whether there's enough info and species are fixed
-def determine_base(bases,minread,thresh):
-    if(len(bases)< minread): #not enough info
-        base='N'
-    else:
-        counts = Counter(bases)
-        if len(counts)==1:
-            base=bases[0]
-        else:
-            if counts.most_common(1)[0][1] / float(len(bases)) >= thresh:
-                base=counts.most_common(1)[0][0]
-            else:
-                base='N'
-                
-    return base
-
-def remove_extra(base_list):
-    bases=['A','C','G','T']
-    new_base_list=[]
-    ibase_list = iter(base_list)
-    for b in ibase_list:
-        if b in bases:
-            new_base_list.append(b)         #get base
-        elif b=='+':                        #skip insertions
-            i = int(ibase_list.next())
-            while i>0:
-                z=ibase_list.next()
-                i = i-1
-        elif b=='-':                        #get deletion
-            i = int(ibase_list.next())
-            while i>0:
-                z=ibase_list.next()
-                i = i-1
-            new_base_list.append('-')
-        elif b=='^':                        #skip read qual noted at end of read
-            z=ibase_list.next()
-    
-    return new_base_list
-
 ###############################################
-path=sys.argv[1]
-minread=int(sys.argv[2])
-thresh=float(sys.argv[3])
-
-allbases=getallbases(path)      #dictionary of combined pileups - locus/pos:bases(as list)
-for pos in allbases:
-    bases = remove_extra(allbases[pos])             #remove indel info
-    base = determine_base(bases,minread,thresh)     #determine if sufficient data and threshold met for calling allele
-    allbases[pos] = base
-
-output = open(path+'/pruned_dict.pkl', 'wb')
+allbases=getallbases(sys.argv[1],sys.argv[2],sys.argv[3])      #dictionary of combined pileups - locus/pos:bases(as list)
+output = open(sys.argv[1]+'/pruned_dict.pkl', 'wb')
 cPickle.dump(allbases, output, cPickle.HIGHEST_PROTOCOL)
 output.close()
