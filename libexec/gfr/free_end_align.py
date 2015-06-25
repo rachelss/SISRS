d708806ad2b66d07d3a83848a07b056c4b72cba3
rachelss <rsschwartz@gmail.com>
2015-06-25 15:08:57 -0700

    fixed folder names

--- libexec/gfr/free_end_align.py
+++ libexec/gfr/free_end_align.py
@@ -235,7 +235,9 @@ allseq2 = [str(s.seq) for s in allseqs ]    #just the sequence (as a string)
 
 seqs=getMSA(allseq2)
 
-outfile = open('alignments/'+sys.argv[1].replace('.fa','_align.fa'),'w')
+folder_name='/'.join(sys.argv[1].split('/')[:-1])
+filename=sys.argv[1].split('/')[0]
+outfile = open(folder_name+'/alignments/'+filename.replace('.fa','_align.fa'),'w')
 for i,a in enumerate(allseqs):
     outfile.write('>'+a.name+"\n")
     outfile.write(seqs[i]+"\n")
