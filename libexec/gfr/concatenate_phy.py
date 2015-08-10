1368499ab866081fb19f28052287bd940dba74c9
rachelss <rsschwartz@gmail.com>
2015-08-10 13:18:04 -0700

    separate MS and MV loci

--- libexec/gfr/concatenate_phy.py
+++ libexec/gfr/concatenate_phy.py
@@ -14,7 +14,7 @@ import glob
 
 ######################
 
-phy_files_file = open(sys.argv[1]+'/phy_files.txt','r')
+phy_files_file = open(sys.argv[1]+'/phy_filesMS.txt','r')
 phy_files = phy_files_file.readlines()
 phy_files_file.close()
 
@@ -34,6 +34,28 @@ for k,v in cat_data.iteritems():
     seq = SeqRecord(Seq(''.join(v)), id=k)
     datalist.append(seq)
 
-SeqIO.write(datalist,sys.argv[1]+'/concat_loci.phylip-relaxed', "phylip-relaxed")
+SeqIO.write(datalist,sys.argv[1]+'/concat_lociMS.phylip-relaxed', "phylip-relaxed")
+
+phy_files_file = open(sys.argv[1]+'/phy_filesMV.txt','r')
+phy_files = phy_files_file.readlines()
+phy_files_file.close()
+
+allspecies = [os.path.basename(sp) for sp in sys.argv[2:]]
+cat_data = {s:list() for s in allspecies}
+
+for f in phy_files:
+    data = SeqIO.to_dict(SeqIO.parse(f.rstrip(), "phylip-relaxed"))
+    for k in allspecies:
+        if k in data:
+            cat_data[k].extend(list(data[k].seq))
+        else:
+            cat_data[k].extend(['N'] * len(data[data.keys()[0]].seq))
+
+datalist=[]
+for k,v in cat_data.iteritems():
+    seq = SeqRecord(Seq(''.join(v)), id=k)
+    datalist.append(seq)
+
+SeqIO.write(datalist,sys.argv[1]+'/concat_lociMV.phylip-relaxed', "phylip-relaxed")
 
 
