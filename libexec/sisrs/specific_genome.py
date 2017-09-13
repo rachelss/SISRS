f87d0a65ffe23b2f01df750d0a77267c9a5ba81b
BobLiterman <jazz710@gmail.com>
2017-09-13 17:06:15 -0400

    Final base calls element, not list

--- libexec/sisrs/specific_genome.py
+++ libexec/sisrs/specific_genome.py
@@ -19,7 +19,7 @@ def getallbases(path):
                 node,pos,ref,num,bases,qual=line.split()
                 loc=node+'/'+pos
                 bases=getCleanList(ref,bases)
-                finalBase=Counter(bases).most_common()
+                finalBase=(Counter(bases).most_common()[0][0])
                 allbases[loc]=finalBase
     return allbases
 
