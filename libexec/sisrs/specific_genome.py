a3f95ac6240fecc68c57c812c6a48c3b9baeee0a
BobLiterman <jazz710@gmail.com>
2017-09-14 10:09:31 -0400

    Added main function

--- libexec/sisrs/specific_genome.py
+++ libexec/sisrs/specific_genome.py
@@ -52,25 +52,26 @@ def getCleanList(ref,bases):
 
 
 ###############################################
-path=sys.argv[1]
-contig_file = sys.argv[2]
+def main(path,contig_file):
+    path=sys.argv[1]
+    contig_file = sys.argv[2]
 
-allbases=getallbases(path)      #dictionary of combined pileups - locus/pos:bases(as list)
-if len(allbases)==0:
-    print 'No data for '+path
-    sys.exit(1)
+    allbases=getallbases(path)      #dictionary of combined pileups - locus/pos:bases(as list)
+    if len(allbases)==0:
+        print 'No data for '+path
+        sys.exit(1)
 
-# Read contig fasta file into dictionary with sequence ID as the key
-contig_handle = open(contig_file, "r")
-fasta_seq = SeqIO.parse(contig_handle, 'fasta')
-fasta_dict = {read.id:list(str(read.seq)) for read in fasta_seq}
-contig_handle.close()
+    # Read contig fasta file into dictionary with sequence ID as the key
+    contig_handle = open(contig_file, "r")
+    fasta_seq = SeqIO.parse(contig_handle, 'fasta')
+    fasta_dict = {read.id:list(str(read.seq)) for read in fasta_seq}
+    contig_handle.close()
 
-for locus_pos,base in allbases.iteritems():
-    locus,pos = locus_pos.split('/')
-    fasta_dict[locus][int(pos)-1] = base
+    for locus_pos,base in allbases.iteritems():
+        locus,pos = locus_pos.split('/')
+        fasta_dict[locus][int(pos)-1] = base
 
-output = open(path+'/contigs.fa', 'wb')
-for l,seq in fasta_dict.iteritems():
-    output.write('>'+str(l)+"\n"+"".join(seq)+"\n")
-output.close()
+    output = open(path+'/contigs.fa', 'wb')
+    for l,seq in fasta_dict.iteritems():
+        output.write('>'+str(l)+"\n"+"".join(seq)+"\n")
+    output.close()
