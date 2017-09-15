ef858401a65218749c9c143ba2323be4d8041481
BobLiterman <jazz710@gmail.com>
2017-09-15 09:15:49 -0400

    Changed test script to find *

--- libexec/sisrs/test_getCleanList.py
+++ libexec/sisrs/test_getCleanList.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2
 from specific_genome import getCleanList
 
-Nef test_answer():
-    assert getCleanList('A','***.......,..^7.^7.^7.^7.^7.^7,') == list('NNNAAAAAAAAAAAAAAAA')
-    assert getCleanList('A','t,.*,.-2TT..,,......,,,.,,,') == list('TAANAAAAAAAAAAAAAAAAAAA')
-    assert getCleanList('G','.,,,,*.$..$.,,,,,,,.') == list('GGGGGNGGGGGGGGGGGG')
+def test_answer():
+    assert getCleanList('A','***.......,..^7.^7.^7.^7.^7.^7,') == list('***AAAAAAAAAAAAAAAA')
+    assert getCleanList('A','t,.*,.-2TT..,,......,,,.,,,') == list('TAA*AAAAAAAAAAAAAAAAAAA')
+    assert getCleanList('G','.,,,,*.$..$.,,,,,,,.') == list('GGGGG*GGGGGGGGGGGG')
     assert getCleanList('T',',+1a,+1a,+1a.+1A.+1A,+1a.+1A') == list('TTTTTTT')
-    assert getCleanList('T','AAaA*A*aA**aaAa') == list('AAAANANAANNAAAA')
+    assert getCleanList('T','AAaA*A*aA**aaAa') == list('AAAA*A*AA**AAAA')
     assert getCleanList('T','..,....,,,,.,') == list('TTTTTTTTTTTTT')
     assert getCleanList('A',',,....+18AGTTAACCCTAAGGGACC,+18agttaaccctaagggacc,+18agttaaccctaagggacc') == list('AAAAAAAA')
