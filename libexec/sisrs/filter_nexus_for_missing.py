6eaec2badbbb504371fc45176eeeff54001b2bb7
BobLiterman <jazz710@gmail.com>
2017-10-13 12:04:40 -0400

    Changed print statement

--- libexec/sisrs/filter_nexus_for_missing.py
+++ libexec/sisrs/filter_nexus_for_missing.py
@@ -52,5 +52,6 @@ SeqIO.write(datalist, path.dirname(sys.argv[1])+'/'+path.basename(sys.argv[1]).s
 locfile = open(path.dirname(sys.argv[1])+'/locs_m'+sys.argv[2]+'.txt', 'w')
 locfile.write("\n".join(newlocs))
 locfile.close()
-loss = (((len(locs)-2)-len(newlocs))/(len(locs)-2)*100
-print 'With '+str(missing)+' taxa allowed to be missing, '+str(len(locs)-2)+' sites from '+path.basename(sys.argv[1])+' ('+str(len(species)-2)+' allowed missing) are reduced to '+str(len(newlocs))+' sites ('+(len(locs)-2)-len(newlocs)+' sites or '+str('%.2f' % loss)+'% lost)'
+origLength = len(data[species[0]])
+newLength = len(newlocs)
+print 'With '+str(missing)+' taxa allowed to be missing, '+str(origLength)+' sites from '+path.basename(sys.argv[1])+' ('+str(len(species)-2)+' allowed missing) are reduced to '+str(len(newlocs))+' sites ('+(len(data[species[0]]))-len(newlocs)+' sites or '+str('%.2f' % ((len(data[species[0]]))-len(newlocs)/(len(data[species[0]]))))+'% lost)'
