a4ca0f346f7062494952ec60cb6f97d7c91bedb0
Anders Pitman <tapitman11@gmail.com>
2017-11-09 18:31:35 -0700

    Change to python 2 and start implementing
    * Using python 2 for now since that what SISRS currently uses.
    * Add Click library for CLI parsing
    * Port directory setup to python

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    # TODO: upgrade to python 3
+#with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(
     name='sisrs',
     version='0.0.1',
+    packages=find_packages(),
     description="Site Identification from Short Read Sequences.",
     long_description=long_description,
     url='https://cartwrig.ht/software/',
-    python_requires='>=3.6',
+    install_requires=[
+        'click',
+    ],
+    # TODO: upgrade to python 3
+    #python_requires='>=3.6',
+    python_requires='>=2.7',
     entry_points={
         'console_scripts': [
             'sisrs-python = sisrs:main'
         ],
     },
-    scripts=['bin/sisrs']
+    scripts=[
+        'bin/sisrs',
+        'libexec/sisrs/sub_sample_for_velvet_unshuff.py'
+    ]
 )
--- sisrs/__init__.py
+++ sisrs/__init__.py
@@ -1,6 +1,77 @@
 import subprocess
+import os
 import sys
+import click
+import shutil
+from click import echo
+from glob import glob
+#import sub_sample_for_velvet_unshuff
+
+def setup_output_directory(data_directory, output_directory, overwrite):
+
+    if output_directory is None:
+        output_directory = data_directory
+        echo("Note: SISRS writing into data folder")
+    elif not os.path.exists(output_directory):
+        recursive_symlinks(data_directory, output_directory)
+    else:
+        if overwrite:
+            echo("{} already exists. Overwriting...".format(
+                output_directory))
+            recursive_symlinks(data_directory, output_directory)
+        else:
+            echo("{} already exists and overwrite flag not set. Aborting...".format(
+                output_directory))
+
+    return os.path.abspath(output_directory)
+
+# based off https://stackoverflow.com/a/15824216/943814
+def recursive_symlinks(src, dest):
+    if os.path.isdir(src):
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        files = glob(os.path.join(src, '*'))
+        for f in files:
+            f = os.path.basename(f)
+            next_src = os.path.join(src, f)
+            next_dest = os.path.join(dest, f)
+            recursive_symlinks(
+                next_src, next_dest)
+    else:
+        try:
+            os.symlink(src, dest)
+        except OSError as e:
+            os.remove(dest)
+            os.symlink(src, dest)
+
+
+@click.group()
+@click.option('--overwrite', type=bool, default=False)
+@click.option(
+    '--data-directory', '-f', type=click.Path(file_okay=False),
+    help='Data directory')
+@click.option(
+    '--output-directory', '-z', type=click.Path(file_okay=False),
+    help='Output directory')
+@click.pass_context
+def cli(ctx, data_directory, output_directory, overwrite):
+
+    if data_directory is None:
+        data_directory = os.getcwd()
+    data_directory = os.path.abspath(data_directory)
+
+    output_directory = setup_output_directory(
+        data_directory, output_directory, overwrite)
+
+    ctx.obj['data_dir'] = data_directory
+    ctx.obj['out_dir'] = output_directory
+
+@cli.command(name='subsample')
+@click.option('--genome-size', '-g', required=True, type=int, help='Genome size')
+@click.pass_context
+def subsample(ctx, genome_size):
+    #print(ctx.obj['data_dir'])
+    pass
 
 def main():
-    command = ['sisrs'] +  sys.argv[1:]
-    subprocess.run(command)
+    cli(obj={})
