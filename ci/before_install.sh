d83b2b7f95dec8a2f52c0290710683d780ef314f
Anders Pitman <tapitman11@gmail.com>
2017-11-15 16:34:00 -0700

    Add first actual test case

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# install dependencies available as Ubuntu packages
 sudo add-apt-repository --yes ppa:debian-med/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq samtools bowtie2 python-biopython parallel mafft
-wget https://sourceforge.net/projects/bbmap/files/BBMap_37.56.tar.gz
-tar xzvf BBMap_37.56.tar.gz
+
+# install BBMap
+wget https://sourceforge.net/projects/bbmap/files/BBMap_37.66.tar.gz
+tar xzvf BBMap_37.66.tar.gz
 export PATH=$PATH:~/bbmap/
 
-
+# download test data
+git clone git@github.com:BobLiterman/SISRS_Small_Data
---
+++ ci/run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bin/sisrs outputAlignment -a premade -c 0 -f ~/SISRS_Small_Data/2_identifyFixedSites/ -z output/
+cmp output/3_outputAlignment/alignment.nex SISRS_Small_Data/3_outputAlignment/alignment.nex
+cmp output/3_outputAlignment/alignmentbi.nex SISRS_Small_Data/3_outputAlignment/alignmentbi.nex
+cmp output/3_outputAlignment/alignmentpi.nex SISRS_Small_Data/3_outputAlignment/alignmentpi.nex
