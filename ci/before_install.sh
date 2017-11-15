9cc20f51789ff6545acba4f22fe951b0282c0cea
Anders Pitman <tapitman11@gmail.com>
2017-11-15 16:41:07 -0700

    Export bbmap binary path

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -8,7 +8,6 @@ sudo apt-get install -qq samtools bowtie2 python-biopython parallel mafft
 # install BBMap
 wget https://sourceforge.net/projects/bbmap/files/BBMap_37.66.tar.gz
 tar xzvf BBMap_37.66.tar.gz
-export PATH=$PATH:~/bbmap/
 
 # download test data
 git clone https://github.com/BobLiterman/SISRS_Small_Data
--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$PATH:~/bbmap/
+
 bin/sisrs outputAlignment -a premade -c 0 -f ~/SISRS_Small_Data/2_identifyFixedSites/ -z output/
 cmp output/3_outputAlignment/alignment.nex SISRS_Small_Data/3_outputAlignment/alignment.nex
 cmp output/3_outputAlignment/alignmentbi.nex SISRS_Small_Data/3_outputAlignment/alignmentbi.nex
