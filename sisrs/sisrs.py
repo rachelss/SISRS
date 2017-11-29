5e0105400b378a27d21f469c232dbaf8febac8d6
Anders Pitman <tapitman11@gmail.com>
2017-11-28 23:05:19 -0700

    Snapshot - start implementing align_contigs

--- sisrs/sisrs.py
+++ sisrs/sisrs.py
@@ -1,4 +1,5 @@
 import subprocess
+from subprocess import Popen, PIPE
 import os
 import sys
 import click
@@ -21,7 +22,7 @@ class DirectoryLists(object):
                     file_path = os.path.join(root, filename)
                     self._all_fastq.append(file_path)
 
-        self._all_dirs = list(set([ os.path.dirname(x) for x in self._all_fastq ]))
+        self._all_dirs = sorted(list(set([ os.path.dirname(x) for x in self._all_fastq ])))
 
     def get_all_dirs(self):
         return self._all_dirs
@@ -75,8 +76,11 @@ def recursive_symlinks(src, dest):
     '--output-directory', '-z', type=click.Path(file_okay=False),
     help='Output directory')
 @click.option('--assembler', '-a', default='velvet', help="Assembler")
+@click.option('--continuous', '-c', default=1, help="Coninuous mode")
+@click.option('--num-processors', '-p', default=1, help="Number of processors")
 @click.pass_context
-def cli(ctx, assembler, data_directory, output_directory, overwrite):
+def cli(ctx, assembler, data_directory, output_directory, overwrite,
+        continuous, num_processors):
 
     if data_directory is None:
         data_directory = os.getcwd()
@@ -87,10 +91,16 @@ def cli(ctx, assembler, data_directory, output_directory, overwrite):
 
     dir_lists = DirectoryLists(output_directory)
 
+    contig_dir = ''
+    if assembler == 'premade':
+        contig_dir = 'premadeoutput'
+
     ctx.obj['data_dir'] = data_directory
     ctx.obj['out_dir'] = output_directory
     ctx.obj['assembler'] = assembler
     ctx.obj['dir_lists'] = dir_lists
+    ctx.obj['contig_dir'] = os.path.join(output_directory, contig_dir)
+    ctx.obj['num_processors'] = num_processors
 
 @cli.command()
 @click.option('--genome-size', '-g', required=True, type=int, help='Genome size')
@@ -111,6 +121,74 @@ def output_alignment(ctx):
     num_missing = len(all_dirs) - 2
     get_alignment(num_missing, 'X', out_dir, assembler)
 
+@cli.command()
+@click.pass_context
+def align_contigs(ctx):
+
+    print("==== Renaming Scaffolds for SISRS ====")
+    contig_dir = ctx.obj['contig_dir']
+    dir_lists = ctx.obj['dir_lists']
+    num_processors = ctx.obj['num_processors']
+
+    contig_file_path = os.path.join(contig_dir, 'contigs.fa')
+
+    # backup contigs.fa
+    backup_contig_file_path = os.path.join(
+        contig_dir, 'contigs_OriginalNames.fa')
+    shutil.move(contig_file_path, backup_contig_file_path)
+
+    # rename scaffolds
+    rename_command = [
+        'rename.sh',
+        'in={}'.format(backup_contig_file_path),
+        'out={}'.format(contig_file_path),
+        'prefix=SISRS',
+        'addprefix=t'
+    ]
+    subprocess.check_call(rename_command)
+    print("==== Scaffolds Renamed ====")
+
+    all_dirs = dir_lists.get_all_dirs()
+    contig_prefix = os.path.join(contig_dir, 'contigs')
+
+    # Build index
+    build_command = [ 'bowtie2-build', contig_file_path, contig_prefix ]
+    subprocess.check_call(build_command)
+
+    for dir_ in all_dirs:
+
+        basename = os.path.basename(dir_)
+        print("==== Aligning {} as Single-Ended ====".format(basename))
+        fastq_filepaths = glob(os.path.join(dir_, '*.fastq'))
+
+        bowtie2_command = [
+            'bowtie2',
+            '-p', str(num_processors),
+            '-N', '1',
+            '--local',
+            '-x', contig_prefix,
+            '-U', ','.join(fastq_filepaths)
+        ]
+
+        #with open('bowtie_out.txt', 'w') as f:
+            #bowtie2_proc = Popen(bowtie2_command, stdout=f)
+            #bowtie2_proc.wait()
+
+        bowtie2_proc = Popen(bowtie2_command, stdout=PIPE)
+
+        samtools_to_bam_command = [
+            'samtools', 'view', '-Su',
+            '-@', str(num_processors),
+            '-F' , '4',
+            '-',
+        ]
+        samtools_to_bam_proc = Popen(
+            samtools_to_bam_command, stdin=bowtie2_proc.stdout,
+            )
+
+
+        bowtie2_proc.wait()
+        samtools_to_bam_proc.wait()
 
 def main():
     cli(obj={})
--- tests/test_sisrs.py
+++ tests/test_sisrs.py
@@ -2,9 +2,9 @@ import os
 import subprocess
 import shutil
 from filecmp import cmp
-from os.path import join
+from os.path import join, exists
 
-data_base_dir = 'test_data'
+data_base_dir = 'pipeline_stages'
 out_base_dir = 'output'
 
 print("out:")
@@ -16,21 +16,66 @@ if os.path.exists(out_base_dir):
 def run(*args, **kwargs):
     return subprocess.check_call(*args, **kwargs)
 
+def test_align_contigs():
 
-def test_output_alignment():
-
-    data_dir = join(data_base_dir, '2_identifyFixedSites')
-    exp_dir = join(data_base_dir, '3_outputAlignment')
+    data_dir = join(data_base_dir, '0_RawData_PremadeGenome')
+    exp_dir = join(data_base_dir, '1_alignContigs')
     out_dir = out_base_dir
+    contig_dir = join(out_dir, 'premadeoutput')
 
     command = [
         'sisrs-python',
+        '-p', '1',
+        '-a', 'premade',
+        '-c', '0',
         '-f', data_dir,
         '-z', out_dir,
-        'output_alignment'
+        'align_contigs'
     ]
     run(command)
 
-    assert cmp(
-        join(out_dir, 'alignment.nex'),
-        join(exp_dir, 'alignment.nex'))
+    # verify contigs.fa backed up
+    assert(exists(join(contig_dir, 'contigs_OriginalNames.fa')))
+
+    contig_file_path = join(contig_dir, 'contigs.fa')
+    with open(contig_file_path, 'r') as f:
+        first_line = f.next()
+
+    # verify renaming
+    assert(first_line.startswith('>SISRS_'))
+
+    # index files generated
+    index_filepaths = [
+        join(contig_dir, filename) for filename in [
+            'contigs.1.bt2',
+            'contigs.2.bt2',
+            'contigs.3.bt2',
+            'contigs.4.bt2',
+            'contigs.rev.1.bt2',
+            'contigs.rev.2.bt2',
+        ]
+    ]
+
+    for index_filepath in index_filepaths:
+        assert(exists(index_filepath))
+
+    
+
+
+#def test_output_alignment():
+#
+#    data_dir = join(data_base_dir, '2_identifyFixedSites')
+#    exp_dir = join(data_base_dir, '3_outputAlignment')
+#    out_dir = out_base_dir
+#
+#    command = [
+#        'sisrs-python',
+#        '-f', data_dir,
+#        '-z', out_dir,
+#        'output_alignment'
+#    ]
+#    run(command)
+#
+#    assert cmp(
+#        join(out_dir, 'alignment.nex'),
+#        join(exp_dir, 'alignment.nex'))
