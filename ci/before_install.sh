bdfde96b7733fd0545026b9afa5eea899672fdf4
Anders Pitman <tapitman11@gmail.com>
2017-12-21 11:43:40 -0700

    CI: Remove sudo from commands
    Assume commands will run as root from a CI environment such as
    Travis

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # install dependencies available as Ubuntu packages
-sudo add-apt-repository --yes ppa:debian-med/ppa
-sudo apt-get update -qq
-sudo apt-get install -qq samtools bowtie2 parallel mafft default-jre
+add-apt-repository --yes ppa:debian-med/ppa
+apt-get update -qq
+apt-get install -qq samtools bowtie2 parallel mafft default-jre
 
 # install BBMap
 wget https://sourceforge.net/projects/bbmap/files/BBMap_37.66.tar.gz
