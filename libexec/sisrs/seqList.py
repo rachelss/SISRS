4dd029c0e5ab94ccf8b39a5835e50f4b2ee9b3c1
BobLiterman <jazz710@gmail.com>
2018-01-12 14:25:09 -0500

    Removed main function, Switched loop to streaming

--- libexec/sisrs/seqList.py
+++ libexec/sisrs/seqList.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python2
 import os
 import sys
-import pandas as pd
 ##############
 
 def createPosList(path,assembler):
-    #Read in sequence lengths from SISRS output in <PATH>/<assembler>output/contigs_SeqLength.tsv; Sort by contig ID
-    df = pd.read_csv(path+"/"+assembler+"output/contigs_SeqLength.tsv",sep="\t",header=None)
-    df.sort_values([0],inplace=True)
-    df=df.reset_index(drop=True)
-
-    #Fill in list
-    j=0
-    posList = []
-    for row in df.iterrows():
-        contig=row[1][0]
-        length=row[1][1]
-        for i in range(1,length+1):
-            pos = contig + "/" + str(i)
-            posList.insert(j,pos)
-            j+=1
-
-    #Write file to <PATH>/<assembler>output/contigs_PosList
-    printList = open(path+"/"+assembler+"output/contigs_LocList", 'w')
+    posList=[]
+    with open(path+"/"+assembler+"output/contigs_SeqLength.tsv","r") as filein:
+        for line in iter(filein):
+            splitline=line.split()
+            lengthList=range(1,(int(splitline[1])+1))
+            for x in lengthList:
+                posList.append(splitline[0] +'/'+str(x))
+    printList = open(path+'/'+assembler+'output/contigs_LocList2','w')
     for item in posList:
-        print>>printList, item
+        print>>printList,item
     printList.close()
     sys.stdout.write("Site list created: " + str(len(posList)) + " total sites\n")
 
+######################################################
 
-if __name__ == "__main__":
-
-    #Read in arguments
-    path = sys.argv[1]
-    assembler = sys.argv[2]
-    createPosList(path,assembler)
+path = sys.argv[1]
+assembler = sys.argv[2]
+createPosList(path,assembler)
