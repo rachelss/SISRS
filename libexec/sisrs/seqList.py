7a779e28b857490b1f8d0a663eb970d50a0b9767
BobLiterman <jazz710@gmail.com>
2018-01-23 09:37:09 -0500

    Fixed counter

--- libexec/sisrs/seqList.py
+++ libexec/sisrs/seqList.py
@@ -6,13 +6,14 @@ import sys
 def createPosList(path,assembler):
     printList = open(path+'/'+assembler+'output/contigs_LocList','w')
     siteCount=0
+    idx=0
     with open(path+"/"+assembler+"output/contigs_SeqLength.tsv","r") as filein:
         for line in iter(filein):
+            siteCount+=idx
             splitline=line.split()
             lengthList=range(1,(int(splitline[1])+1))
             for idx,x in enumerate(lengthList):
                 print>>printList,splitline[0] +'/'+str(x)
-            siteCount+=idx
     printList.close()
     sys.stdout.write("Site list created: " + str(siteCount) + " total sites\n")
 
