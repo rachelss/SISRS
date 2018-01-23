f66bd72976e08e3b0dbbdf354a683af2c9a002c3
BobLiterman <jazz710@gmail.com>
2018-01-23 09:25:57 -0500

    Removed unnecessary list

--- libexec/sisrs/seqList.py
+++ libexec/sisrs/seqList.py
@@ -4,16 +4,14 @@ import sys
 ##############
 
 def createPosList(path,assembler):
-    posList=[]
+    printList = open(path+'/'+assembler+'output/contigs_LocList','w')
+
     with open(path+"/"+assembler+"output/contigs_SeqLength.tsv","r") as filein:
         for line in iter(filein):
             splitline=line.split()
             lengthList=range(1,(int(splitline[1])+1))
             for x in lengthList:
-                posList.append(splitline[0] +'/'+str(x))
-    printList = open(path+'/'+assembler+'output/contigs_LocList','w')
-    for item in posList:
-        print>>printList,item
+                print>>printList,splitline[0] +'/'+str(x)
     printList.close()
     sys.stdout.write("Site list created: " + str(len(posList)) + " total sites\n")
 
