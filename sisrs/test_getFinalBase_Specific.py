9dae267e14d64a9ab26b0a7e60de1ea8053f395a
BobLiterman <jazz710@gmail.com>
2018-04-03 10:45:43 -0400

    Updated test

--- sisrs/test_getFinalBase_Specific.py
+++ sisrs/test_getFinalBase_Specific.py
@@ -4,7 +4,7 @@ from .specific_genome import getFinalBase_Specific
 
 def test_answer():
 
-    #Test getFinalBase for specific_genome and get_pruned_dict (handle deltions differently and minread/thresh)
+    #Test getFinalBase for specific_genome
     assert getFinalBase_Specific(getCleanList('A','***.......,..^7.^7.^7.^7.^7.^7,'))=='A'
     assert getFinalBase_Specific(getCleanList('A','t,.*,.-2TT..,,......,,,.,,,'))=='A'
     assert getFinalBase_Specific(getCleanList('G','.,,,,*.$..$.,,,,,,,.'))=='G'
@@ -14,5 +14,11 @@ def test_answer():
     assert getFinalBase_Specific(getCleanList('A',',,....+18AGTTAACCCTAAGGGACC,+18agttaaccctaagggacc,+18agttaaccctaagggacc'))=='A'
     assert getFinalBase_Specific(getCleanList('T','***.*,*.***,*'))=='N'
 
+    #Test how to handle ties
+    assert getFinalBase_Specific(getCleanList('T','TtTtTAAaAa'))=='A'
+    assert getFinalBase_Specific(getCleanList('T','GgGggAAaAa'))=='A'
+    assert getFinalBase_Specific(getCleanList('T','TtTtTgGggG'))=='G'
+
     #Test how to handle deletions
     assert getFinalBase_Specific(getCleanList('T','*************'))=='N'
+    assert getFinalBase_Specific(getCleanList('T','*****AAaAa'))=='A'
